a2bce7e80dc5901be55bb42ee960edfdfd56bfa8
Abrar Trabulsi <abrartrabulsi@college.harvard.edu>
2019-11-28 23:18:43 -0600

    final commit of the night, going to work on building out my shiny app tomorrow

--- regime_vs_economic_shiny/regime_economic_prep.R
+++ regime_vs_economic_shiny/regime_economic_prep.R
@@ -19,7 +19,7 @@ mymap4 <- read_rds("regime_vs_economic_shiny/mymap4.rds")
 anim_save("regime_vs_economic_shiny/mymap1.gif", mymap1)
 anim_save("regime_vs_economic_shiny/mymap2.gif", mymap2)
 
-# saving my last ggplot. using ggsave saves your last plot as whatever you tell it to!
+# saving my non-animated ggplots as images! just tell it what kind of file you want it to be then the named object you're wanting to convert
 
 ggsave("mymap3.png", plot = mymap3)
 ggsave("mymap4.png", plot = mymap4)
